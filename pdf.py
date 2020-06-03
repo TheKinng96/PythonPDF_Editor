3b0dd778f83adcaf6ff3ae37cbdfc54bbf2e1b2b
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-03 15:20:23 +0900

    practiced merger

---
+++ pdf.py
@@ -0,0 +1,22 @@
+import PyPDF2
+import sys
+
+inputs = sys.argv[1:]
+
+def pdf_conbiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        merger.append(pdf)
+    merger.write('super.pdf')
+
+pdf_conbiner(inputs)
+
+# mode: r → give us back binary; rb → read binary
+# with open('dummy.pdf','rb') as file:
+#     reader = PyPDF2.PdfFileReader(file)
+#     page = reader.getPage(0)
+#     page.rotateCounterClockwise(90)
+#     writer = PyPDF2.PdfFileWriter()
+#     writer.addPage(page)
+#     with open('tilt.pdf', 'wb') as new_file:
+#         writer.write(new_file)
